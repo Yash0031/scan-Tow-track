de933782ec20f0b56c39f3f1a458e9358660b739
Yash0031 <yash31102000@gmail.com>
2021-02-02 14:48:47 +0530

    Authentication

--- app/src/main/java/com/example/scantowtrack/LoginPage.java
+++ app/src/main/java/com/example/scantowtrack/LoginPage.java
@@ -1,17 +1,81 @@
 package com.example.scantowtrack;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginPage extends AppCompatActivity {
+
+    private EditText emailId,passWord;
+    private Button loginbtn;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        emailId = findViewById(R.id.email);
+        passWord = findViewById(R.id.password);
+        loginbtn = findViewById(R.id.login);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+    public void login(){
+        String eMail = emailId.getText().toString();
+        String pass = passWord.getText().toString();
+        if (eMail.matches("")) {
+            Toast.makeText(this, "Please Enter Email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        firebaseAuth.signInWithEmailAndPassword(eMail, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(LoginPage.this, "Sign in failed", Toast.LENGTH_LONG).show();
+                            updateUI(null);
+
+                        }
+                    }
+                });
+
+    }
+    public  void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+    private void updateUI(FirebaseUser currentUser) {
+        if(currentUser != null) {
+            Intent intent = new Intent(this, previewBeforeTowing.class);
+            startActivity(intent);
+        }
+
     }
 
     public void loginMe(View view) {
