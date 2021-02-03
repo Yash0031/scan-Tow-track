1fddc4505d4bf0768fbde1ace6b942147f41aa8a
Yash0031 <yash31102000@gmail.com>
2021-02-03 16:14:52 +0530

    CheckStatus

--- app/src/main/java/com/example/scantowtrack/checkVehicle.java
+++ app/src/main/java/com/example/scantowtrack/checkVehicle.java
@@ -1,21 +1,44 @@
 package com.example.scantowtrack;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class checkVehicle extends AppCompatActivity {
 
+    EditText num;
+    String n;
+    Button check;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_vehicle);
+        num = findViewById(R.id.vehicleNumber);
+        check = findViewById(R.id.checkForTowing);
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String noPlate = num.getText().toString();
+                Intent intent = new Intent(checkVehicle.this,statusOfTowing.class);
+                intent.putExtra("numberPlate",noPlate);
+                startActivity(intent);
+            }
+        });
     }
 
-    public void checkForTow(View view) {
-        Intent intent = new Intent(this, statusOfTowing.class);
-        startActivity(intent);
-    }
+
+
+
 }
--- app/src/main/java/com/example/scantowtrack/statusOfTowing.java
+++ app/src/main/java/com/example/scantowtrack/statusOfTowing.java
@@ -1,14 +1,61 @@
 package com.example.scantowtrack;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class statusOfTowing extends AppCompatActivity {
+
+    TextView name,veh,fine;
+    DatabaseReference reff;
+    String No;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status_of_towing);
+
+        name =(TextView)findViewById(R.id.owname);
+        veh = (TextView)findViewById(R.id.vehno);
+        fine = (TextView)findViewById(R.id.fine);
+        No = getIntent().getStringExtra("numberPlate");
+
+        onStart();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        reff = FirebaseDatabase.getInstance().getReference().child(No);
+        reff.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String oname = snapshot.child("Name").getValue().toString();
+                String fn = snapshot.child("Fine").getValue().toString();
+                String vn = snapshot.child("vehno").getValue().toString();
+                name.setText(oname);
+                fine.setText(fn);
+                veh.setText(vn);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
 }
